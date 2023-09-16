1949f23b1e0b1b9b33b27523c9b72727e17f5b3d
Karthik07nk <117428424+Karthik07nk@users.noreply.github.com>
2023-09-16 21:57:47 +0530

    commit

--- full-stack-backend/src/main/java/com/karthik/fullstack/backend/execption/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.karthik.fullstack.backend.execption;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("could not found the user with this id "+id);
-    }
-
-}
